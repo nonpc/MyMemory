67628d2b5b2a02320c7bceca324b97441e8cb637
nonpc <weibinDS@126.com>
2019-07-10 10:30:31 +0800

    05

---
+++ 替换空格.cpp
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+using namespace std;
+
+
+//41.将字符串中的空格替换为#25
+/*
+解题思路:
+先遍历一遍字符串,看有几个空格,然后动态开辟新的数组,将字符串从后向前写入新的数组中
+*/
+int Job(char *arr)
+{
+	assert(arr != NULL);
+	int count = 0;
+	while(*arr != '\0')
+	{
+		if(*arr == ' ')
+		{
+			count++;
+		}
+		arr++;
+	}
+	return count;
+}
+char* Job1(char *arr,int len,char *brr)
+{
+	assert(arr != NULL);
+	while(*arr != '\0')
+	{
+		if(*arr == ' ')
+		{
+			*brr = '#';
+			brr++;
+			*brr = '2';
+			brr++;
+			*brr = '5';
+			brr++;
+			arr++;
+		}
+		else
+		{
+			*brr = *arr;
+			brr++;
+			arr++;
+		}
+	}
+	*brr = '\0';
+	return brr;
+}
+int main()
+{
+	char arr[] = "i am tulun";
+	int len = sizeof(arr)/sizeof(arr[0]);
+	char *brr = (char *)malloc(sizeof(char)*(len + Job(arr)*3));
+	Job1(arr,len,brr);
+	printf("%s\n",brr);
+
+	return 0;
+}
