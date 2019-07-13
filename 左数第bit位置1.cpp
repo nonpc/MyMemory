ceebfd9dea0742fc4d1b9ad4040bc433c844e0a9
nonpc <weibinDS@126.com>
2019-07-13 14:26:52 +0800

    移位

---
+++ 左数第bit位置1.cpp
@@ -0,0 +1,15 @@
+//27.×óÊıµÚ bit Î» ÖÃ 1
+void Job(int num,int bit)
+{
+	int n=  pow((double)2,(32-bit));    //  num | 2^(32-k)
+	num = num | n;
+	printf("%d\n",num);
+}
+
+
+int main()
+{
+	Job(5,31);
+
+	return 0;
+}
