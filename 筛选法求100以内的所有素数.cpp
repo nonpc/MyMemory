f947004b427324d626dfe1ce5ace49573c3a6d73
nonpc <weibinDS@126.com>
2019-07-13 14:17:11 +0800

    字符串问题

---
+++ 筛选法求100以内的所有素数.cpp
@@ -0,0 +1,36 @@
+//21.筛选法求100以内的所有素数
+void Job1()
+{
+	int arr[101]={0};
+	for(int i=0;i<100;i++)
+	{
+		arr[i]=i+1;
+	}
+	for(int j=3;j<100;j++)//从第三个元素，即4开始
+	{
+		for(int i=2;i<j;i++)
+		{
+			if(arr[j]%i==0)//素数赋值为0
+			{
+				arr[j]=0;
+			}
+		}
+	}
+	printf("100以内的素数有：\n");
+	for(int i=0;i<100;i++)
+	{
+		if(arr[i]==0)//元素为0的跳过
+		{
+			continue;
+		}
+		printf("%d,",arr[i]);
+	}
+	printf("\n");
+}
+
+
+int main()
+{
+	Job1();
+	return 0;
+}
