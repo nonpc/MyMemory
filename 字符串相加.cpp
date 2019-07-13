8d28a5326dd45e8b98a795278e5630439dcbdef8
nonpc <weibinDS@126.com>
2019-07-13 14:20:57 +0800

    字符串问题

---
+++ 字符串相加.cpp
@@ -0,0 +1,23 @@
+//22.字符串相加Hello world! 102
+void Job1()
+{
+	char arr[40];
+	signed char checksum=-1;
+	int i=0;
+	printf("输入字符串：");
+	gets(arr);
+	while(arr[i]!='\0')
+	{
+		checksum+=arr[i];
+		i++;
+	}
+	checksum+='\n';//回车
+	printf("输出结果：%s  %d\n",arr,checksum);
+}
+
+
+int main()
+{
+	Job1();
+	return 0;
+}
